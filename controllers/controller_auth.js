0a2fe180a10e115c369e1d2db8a2facf0f094792
william <wdelaespriella@hotmail.com>
2019-07-22 18:38:18 -0500

    add auth controller

--- controllers/controller_auth.js
+++ controllers/controller_auth.js
@@ -1,86 +1,111 @@
 'use strict'
 
-const Platform = require('../models/platforms')
+const User = require('../schemas/user')
+const fcm = require('../services/fcmTopic')
+const service = require('../services')
+const bcrypt=require('bcrypt-nodejs')
 
-const sign_up = async (req, res, next) => {
-    if (req.body.password !== req.body.password_confirmation) {
-        return res.status(422).json({ error: 'Contraseñas no coinciden!' })
-    }
-    console.log(req.body)
-    const platform = new Platform({
-        full_name: req.body.full_name,
-        email: req.body.email,
-        compania: req.body.compania,
-        password: req.body.password
+
+function signUp (req, res) {
+  
+  bcrypt.genSalt(10,(err,salt)=>{
+    if(err) res.status(503).send({message:err})
+
+    bcrypt.hash(req.body.password,salt,null,(err,hash)=>{
+      if (err) res.status(503).send({message:err})
+
+          const user = new User({
+               email: req.body.email,
+               displayName: req.body.displayName,
+               password: hash,
+               token:req.body.token
+           })
+
+         user.save((err,user) => {
+           if (err) return res.status(503).json(`Error al crear el usuario: ${err}` )
+             console.log(user)
+      res.status(200).json(service.createTokenUser(user))
+     })
     })
-    try {
-        let p = await platform.save()
-        console.log(p.toAuthJSON())
-        res.status(200).json(p.toAuthJSON())
-    } catch (error) {
-        next(error)
-    }
+
+  })
+ 
 }
 
-const sign_in = async (req, res, next) => {
-    let email = req.body.email
-    let password = req.body.password
-    try {
-        let platform = await Platform.findOne({ email: email })
-        if (platform.validPassword(password)) {
-            res.status(200).json(platform.toAuthJSON())
-        } else {
-            res.status(423).json({ error: "Email o contraseña no encontrado" })
+function signIn (req, rest) {
+  User.find({ email: req.body.email }, (err, user) => {
+    if (err) return rest.status(500).send({ message: err })
+    if (user.length == 0) return rest.status(203).json('Email no encontrado.' )
+
+bcrypt.compare(req.body.password, user[0].password, function(err, res) {
+   if (err) return rest.status(500).json(err)
+    if(res){
+        req.user = user
+        if(function_name(user[0].token,req.body.token)){
+           console.log(user)
+            rest.status(200).json(service.createTokenUser(user[0]))
+        }else{
+           user[0].token.push(req.body.token)
+          user[0].save((err,userx)=>{
+              console.log(userx)
+          
+           rest.status(200).json(service.createTokenUser(user[0]))
+          
+        })
         }
-
-    } catch (error) {
-        res.status(423).json({ error: "Email o contraseña no encontrado" })
+       
+       
+    }else{
+        rest.status(203).json('contraseña incorrecta.')
     }
+   
+})
+    
+  })
 }
-// function cambiarContraseña(req, rest) {
-//     bcrypt.genSalt(10, (err, salt) => {
-//         if (err) res.status(503).send({ message: err })
-//         bcrypt.hash(req.body.password, salt, null, (err, hash) => {
-//             if (err) res.status(503).send({ message: err })
-//             User.findByIdAndUpdate(req.user, { password: hash }, (err, user) => {
-//                 if (err) return rest.status(500).send({ message: err })
-//                 if (user == null) return rest.status(203).json('Email no encontrado.')
+function cambiarContraseña (req, rest) {
+  bcrypt.genSalt(10,(err,salt)=>{
+    if(err) res.status(503).send({message:err})
+     bcrypt.hash(req.body.password,salt,null,(err,hash)=>{
+      if (err) res.status(503).send({message:err})
+      User.findByIdAndUpdate(req.user,{password:hash}, (err, user) => {
+        if (err) return rest.status(500).send({ message: err })
+        if (user == null) return rest.status(203).json('Email no encontrado.' )
 
-//                 rest.status(200).json('Contraseña Cambiada')
+          rest.status(200).json('Contraseña Cambiada')
+
+             
+      })
+
+  })   
+ })
 
 
-//             })
+}
+function cerrarSesion(req,res) {
+ User.findById(req.user,(err,user)=>{
+    if (err) res.status(503).send({message:err})
+     
+    var index = user.token.indexOf(req.body.token);
+     user.token.splice(index, 1);
+       user.save((err,user)=>{
+        if (err) return res.status(500).send({ message: err })
+        res.status(200).json('Gracias por Usar nuestra App')
+       })
+     })   
+}
 
-//         })
-//     })
-
-
-// }
-// function cerrarSesion(req, res) {
-//     User.findById(req.user, (err, user) => {
-//         if (err) res.status(503).send({ message: err })
-
-//         var index = user.token.indexOf(req.body.token);
-//         user.token.splice(index, 1);
-//         user.save((err, user) => {
-//             if (err) return res.status(500).send({ message: err })
-//             res.status(200).json('Gracias por Usar nuestra App')
-//         })
-//     })
-// }
-
-// function function_name(array, name) {
-//     for (var i = array.length - 1; i >= 0; i--) {
-//         if (array[i] == name) {
-//             return true
-//         }
-//     }
-//     return false
-// }
+ function function_name(array,name) {
+  for (var i = array.length - 1; i >= 0; i--) {
+    if(array[i] == name){
+      return true
+    }
+  }
+  return false
+}
 module.exports = {
-    sign_up,
-    sign_in
-    // signIn,
-    // cambiarContraseña,
-    // cerrarSesion
+  signUp,
+  signIn,
+  cambiarContraseña,
+cerrarSesion
 }
